d07a31a53241358057c459a1736b6ceff55f131b
Damian Knopp <tribeknopp@gmail.com>
2014-05-03 11:25:52 -0400

    added more cities compile with java 8

--- src/main/java/dmk/mapquest/model/BoundingBox.java
+++ src/main/java/dmk/mapquest/model/BoundingBox.java
@@ -44,7 +44,10 @@ public class BoundingBox {
 	}
 	
 	public static BoundingBox BALTIMORE;
+	public static BoundingBox COLUMBIA;
 	public static BoundingBox ANNAPOLIS;
+	public static BoundingBox STEVENSVILLE;
+	public static BoundingBox EASTON;
 	static{
 		BALTIMORE = new BoundingBox();
 		BALTIMORE.setBound1("39.503136");
@@ -52,11 +55,29 @@ public class BoundingBox {
 		BALTIMORE.setBound3("39.077998");
 		BALTIMORE.setBound4("-76.337942");
 
+		COLUMBIA = new BoundingBox();
+		COLUMBIA.setBound1("39.426802");
+		COLUMBIA.setBound2("-77.145438");
+		COLUMBIA.setBound3("39.001199");
+		COLUMBIA.setBound4("-76.596121");
+		
 		ANNAPOLIS = new BoundingBox();
 		ANNAPOLIS.setBound1("39.191975");
 		ANNAPOLIS.setBound2("-76.770529");
 		ANNAPOLIS.setBound3("38.764949");
 		ANNAPOLIS.setBound4("-76.221213");
+
+		STEVENSVILLE = new BoundingBox();
+		STEVENSVILLE.setBound1("39.209003");
+		STEVENSVILLE.setBound2("-76.585135");
+		STEVENSVILLE.setBound3("38.782079");
+		STEVENSVILLE.setBound4("-76.035818");
+
+		EASTON = new BoundingBox();
+		EASTON.setBound1("38.988391");
+		EASTON.setBound2("-76.343436");
+		EASTON.setBound3("38.560136");
+		EASTON.setBound4("-75.794119");
 	}
 	
 }
